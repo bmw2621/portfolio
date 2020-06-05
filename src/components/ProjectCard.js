3434be8172bad3b94f7e93d8ec2ec235f293fc5b
Benjamin Winchester <benjamin.m.winchester@googlemail.com>
2020-06-05 16:53:21 -0400

    add art page - modal still needs work

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -6,15 +6,43 @@ const ProjectCard = (props) => {
    
     const cardImage = props.images.allImageSharp.nodes.filter(i => i.fluid.originalName === img)[0]
     
+    const showModal = () => {
+        document.getElementById(`${title}Modal`).style.display = "initial"
+        var allBodyChildren = document.getElementsByClassName('projectCard');
+        for(let i = 0; i < allBodyChildren.length; i++){
+            allBodyChildren[i].style.filter = 'blur(20px)'
+        }
+        document.getElementById(`${title}ID`).style.filter = 'none'
+    }
+    const closeModal = () => {
+        document.getElementById(`${title}Modal`).style.display = "none"
+        var allBodyChildren = document.getElementsByClassName('projectCard');
+        for(let i = 0; i < allBodyChildren.length; i++){
+            allBodyChildren[i].style.filter = 'none'
+        }
+    }
+
+    
     return (
-        <div className="projectCard" style={{}}>
+        <div className="projectCard" id={`${title}ID`} style={{}}>
             <h1>{title}</h1>
-            {cardImage ? <Img fluid={cardImage.fluid} style={{"width": "50%", "minWidth":"100px","borderRadius":"50%","marginBottom":".67em"}} /> : null}
+
+            {cardImage && !props.modal && <Img fluid={cardImage.fluid} objectFit="scale-down" style={{"width": "18vw", "height": "18vw", "minWidth":"100px","borderRadius":"50%","marginBottom":".67em"}} /> }
+            {cardImage && props.modal && <div onClick={()=>showModal()}><Img fluid={cardImage.fluid} objectFit="scale-down" style={{"width": "18vw", "height": "18vw", "minWidth":"100px","borderRadius":"50%","marginBottom":".67em"}} /></div> }
+
             <span>{description}</span>
             <div className="projectButtons">
                 {liveURL ? <a className="projectButton" href={liveURL}>Live Site</a> : null}
                 {codeURL ? <a className="projectButton" href={codeURL}>Code</a> : null}
             </div>
+            { props.modal &&
+                <div className="modalBG" id={`${title}Modal`}>
+                    <div className="modal">
+                    <div className="closeModal" onClick={() => closeModal()}>X</div>
+                        <Img fluid={cardImage.fluid} objectFit="cover" />
+                    </div>
+                </div>
+            }
         </div>
     )
 }
--- src/pages/art.js
+++ src/pages/art.js
@@ -1,21 +1,126 @@
-import React from "react"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPhone } from '@fortawesome/free-solid-svg-icons'
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
-import { faGlobeAmericas } from '@fortawesome/free-solid-svg-icons'
-
-import Img from "gatsby-image"
-
+import React from "react";
+import ProjectCard from '../components/ProjectCard';
+import { useStaticQuery, graphql } from 'gatsby';
 import Layout from "../components/Layout"
 import SEO from "../components/SEO"
 
+const art = [
+  {
+    title: "3rd Combat Aviation Brigade",
+    description: "3D Render of unit logo for 3rd Combat Aviation Brigade",
+    img: '3cab.png',
+    codeURL: null,
+    liveURL: null
+  },
+  {
+    title: "USASOAC",
+    description: "3D Render of unit logo for United States Army Special Operations Aviation Command",
+    img: "usasoac.png",
+    codeURL: null,
+    liveURL: null
+  },
+  {
+    title: "Glass Render",
+    description: "3D Render of glass soda bottle using HDRI environmental lighting",
+    img: "soda.png",
+    codeURL: null,
+    liveURL: null
+  },
+  {
+    title: "#VetsWhoCode",
+    description: "3D Render of Vets Who Code logo, modeled and textured in Blender, live rendering uses Three.js with orbital controls. ",
+    img: "vwc.png",
+    codeURL: null,
+    liveURL: "http://cruel-potato.surge.sh/"
+  },
+  {
+    title: "Mushrooms",
+    description: "Cut mushrooms on a cutting board highlighting complex UV unwrapping and texturing",
+    img: "mushrooms.png",
+    codeURL: null,
+    liveURL: null
+  },
+  {
+    title: "Grip Trainer",
+    description: "3D Render of grip strength training highlighting node based mesh displacement and metalic materials",
+    img: "gripTrainer.png",
+    codeURL: null,
+    liveURL: null
+  },
+  {
+    title: "Bench",
+    description: "35mm Film at Thomas Cooper Library, University of South Carolina",
+    img: "bench.jpg",
+    codeURL: null,
+    liveURL: null
+  },
+  {
+    title: "Lights",
+    description: "35mm Film extended exposure involving light painting",
+    img: "lights.jpg",
+    codeURL: null,
+    liveURL: null
+  },
+  {
+    title: "Temperature",
+    description: "Exercise in using light temperature for color manipulation",
+    img: "temperature.jpg",
+    codeURL: null,
+    liveURL: null
+  },
+  {
+    title: "Faded Glory",
+    description: "35mm Film Exposure",
+    img: "fadedGlory.jpg",
+    codeURL: null,
+    liveURL: null
+  },
+  {
+    title: "Metro 1",
+    description: "Digital Exposure in Washington DC Metro",
+    img: "metro1.jpg",
+    codeURL: null,
+    liveURL: null
+  },
+  {
+    title: "Metro 2",
+    description: "Digital Exposure in Washington DC Metro",
+    img: "metro2.jpg",
+    codeURL: null,
+    liveURL: null
+  },
+  {
+    title: "SPIES",
+    description: "Digital Exposure of military training exercise at Fort Benning, GA (Special Patrol Insertion/Extraction System)",
+    img: "spies.jpg",
+    codeURL: null,
+    liveURL: null
+  },
+
+]
+
 const ArtPage = () => {
+      
+    const images = useStaticQuery(graphql`
+        query ArtQuery {
+            allImageSharp {
+                nodes {
+                    fluid {
+                        originalName
+                        ...GatsbyImageSharpFluid
+                    }
+                }
+            }
+        }
+    `)
  
   return (
     <Layout>
-      <SEO title="Home" />
-      <h1>Artwork</h1>
-      <h2>Under Construction</h2>
+      <SEO title="Art" />
+      <h1>Art</h1>
+      <div className="projectCards">
+        {art.map(project => <ProjectCard data={project} images={images} modal={true}/>)}      
+      </div>
     </Layout>
   )
 }
